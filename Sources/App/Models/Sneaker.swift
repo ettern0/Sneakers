f42279685f895aa2a75d790000ededdee612030e
ettern0 <evserdyukov@gmail.com>
2022-05-12 15:18:25 +0200

    Add new fields to Sneakers model

--- Sources/App/Models/Sneaker.swift
+++ Sources/App/Models/Sneaker.swift
@@ -1,3 +1,10 @@
+//
+//  File.swift
+//
+//
+//  Created by Evgeny Serdyukov on 12.05.2022.
+//
+
 import Fluent
 import Vapor
 
@@ -7,9 +14,48 @@ final class Sneaker: Model, Content {
     @ID(key: .id)
     var id: UUID?
 
+    @Field(key: "idStockX")
+    var idStockX: String
+
+    @Field(key: "styleID")
+    var styleID: String
+
+    @Field(key: "thumbnail")
+    var thumbnail: String
+
+    @Field(key: "colorway")
+    var colorway: String
+
+    @Field(key: "shoeName")
+    var shoeName: String
+
+    @Field(key: "description")
+    var description: String
+
     init() { }
 
-    init(id: String) {
-        self.id = UUID(uuidString: id)
+    init(id: UUID?) {
+        self.id = (id == nil ? UUID() : id)
+    }
+}
+
+extension Sneaker {
+    convenience init(id: UUID? = nil, sneakerDTO: SneakerDTO) {
+        self.init(id: id)
+        self.idStockX = sneakerDTO.urlKey
+        self.styleID = sneakerDTO.styleID
+        self.thumbnail = sneakerDTO.thumbnail
+        self.colorway = sneakerDTO.colorway
+        self.shoeName = sneakerDTO.shoeName
+        self.description = sneakerDTO.description
+    }
+
+    func update(with sneakerDTO: SneakerDTO) {
+        self.idStockX = sneakerDTO.urlKey
+        self.styleID = sneakerDTO.styleID
+        self.thumbnail = sneakerDTO.thumbnail
+        self.colorway = sneakerDTO.colorway
+        self.shoeName = sneakerDTO.shoeName
+        self.description = sneakerDTO.description
     }
 }
