c352979a1dea77fc6caebf05ee8762a9325a2794
ettern0 <evserdyukov@gmail.com>
2022-05-12 09:49:28 +0200

    ADD new API (Sneakers Detail, Sneakers info by pages)

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,13 +2,39 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
+
+    //MARK: Check API
     app.get { req in
         return "It works!"
     }
 
-    app.get("hello") { req -> String in
-        return "Hello, world!"
+    //MARK: Check API
+    app.get("sneakers", ":keyWord", ":page", ":count") { req async throws -> String in
+        var keyWord = ""
+        var count = 1000
+        var page = 1
+        if let param = req.parameters.get("keyWord") {
+            keyWord = param
+        }
+        if let param = req.parameters.get("page") {
+            page = Int(param) ?? page
+        }
+        if let param = req.parameters.get("count") {
+            count = Int(param) ?? count
+        }
+        keyWord = (keyWord == "_" ? "" : keyWord)
+        let result = try await getProductData(keyWord: keyWord, page: page, count: count)
+        return result
     }
 
+    app.get("sneakerInfo", ":urlKey") { req async throws -> String in
+        guard let urlKey = req.parameters.get("urlKey") else {
+            throw Abort(.internalServerError)
+        }
+        let result = try await getProductInfoFromStockX(urlKey: urlKey)
+        return result
+    }
+
+    //MARK: Sneakers DB Controller
     try app.register(collection: SneakersController())
 }
