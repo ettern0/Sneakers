1b1f6f3c06e3e144c289334d1d6e8e444d498979
ettern0 <evserdyukov@gmail.com>
2022-05-12 15:16:45 +0200

    Fix API

--- Sources/App/API/Api.swift
+++ Sources/App/API/Api.swift
@@ -12,36 +12,40 @@ import FoundationNetworking
 
 //DD1391-100
 
-func getProductData(keyWord: String, page:Int = 1, count: Int) async throws -> String {
+func getProductData(keyWord: String, page:Int = 1, count: Int) async throws -> [SneakerDTO] {
 
     do {
         var sneakers = try await getDataFromStockX(keyWord: keyWord, page: page, count: count)
         for i in 0..<sneakers.count {
-            //MARK: additional data from secondary API
-            //let dataFromStadiumGoods = try await getDataFromStadiumgoods(styleID: sneakers[i].styleID)
-            let dataFromGoat = try await getDataFromGoat(styleID: sneakers[i].styleID)
-            sneakers[i].goatProductId = dataFromGoat.goatProductId
-            sneakers[i].resellLinkGoat = dataFromGoat.resellLink
-            sneakers[i].lowestResellPriceGoat = dataFromGoat.lowestResellPrice
 
-            let dataFromFlightClub = try await getDataFromFlightClub(styleID: sneakers[i].styleID)
-            sneakers[i].resellLinkFlightClub = dataFromFlightClub.resellLink
-            sneakers[i].lowestResellPriceFlightClub = dataFromFlightClub.lowestResellPrice
+            //MARK: get 360 representation from stockX
+            let dataFromStocX = try await get360FromStockX(urlKey: sneakers[i].urlKey)
+            sneakers[i].images360 = dataFromStocX
 
-            //MARK: Prices and sizes
-            sneakers[i].resellPricesStockX = try await getPricesFromStockX(urlKey: sneakers[i].urlKey.absoluteString)
-//            let pricesFromStadiumGoods = try await getPricesFromStadiumGoods(urlKey: sneaker.urlKey)
-            sneakers[i].resellPricesGoat = try await getPricesFromGoat(productID: sneakers[i].goatProductId)
-           // sneakers[i].resellPricesFlightClub = try await getPricesFromFlightClub(resellLink: sneakers[i].resellLinkFlightClub, styleID: sneakers[i].styleID)
+            //MARK: TODO additional, comment false
+            if false {
+                //MARK: additional data from secondary API
+                //let dataFromStadiumGoods = try await getDataFromStadiumgoods(styleID: sneakers[i].styleID)
+                let dataFromGoat = try await getDataFromGoat(styleID: sneakers[i].styleID)
+                sneakers[i].goatProductId = dataFromGoat.goatProductId
+                sneakers[i].resellLinkGoat = dataFromGoat.resellLink
+                sneakers[i].lowestResellPriceGoat = dataFromGoat.lowestResellPrice
+
+                let dataFromFlightClub = try await getDataFromFlightClub(styleID: sneakers[i].styleID)
+                sneakers[i].resellLinkFlightClub = dataFromFlightClub.resellLink
+                sneakers[i].lowestResellPriceFlightClub = dataFromFlightClub.lowestResellPrice
+
+                //MARK: Prices and sizes
+                sneakers[i].resellPricesStockX = try await getPricesFromStockX(urlKey: sneakers[i].urlKey)
+                //            let pricesFromStadiumGoods = try await getPricesFromStadiumGoods(urlKey: sneaker.urlKey)
+                sneakers[i].resellPricesGoat = try await getPricesFromGoat(productID: sneakers[i].goatProductId)
+                // sneakers[i].resellPricesFlightClub = try await getPricesFromFlightClub(resellLink: sneakers[i].resellLinkFlightClub, styleID: sneakers[i].styleID)
+            }
+            return sneakers
         }
-
-        let encoder = JSONEncoder()
-        encoder.outputFormatting = .prettyPrinted
-        let jsonData = try encoder.encode(sneakers)
-        return String(decoding: jsonData, as: UTF8.self)
         
     } catch {
         print("Fetching images failed with error \(error)")
     }
-    return ""
+    return []
 }
