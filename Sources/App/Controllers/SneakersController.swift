b461eb1a961691be45152bb2baf87be69b9968bc
ettern0 <evserdyukov@gmail.com>
2022-05-12 15:17:49 +0200

    Fix download API to DB

--- Sources/App/Controllers/SneakersController.swift
+++ Sources/App/Controllers/SneakersController.swift
@@ -16,13 +16,12 @@ struct SneakersController: RouteCollection {
     }
 
     func create(req: Request) async throws -> HTTPStatus {
-        var count = 1000
+        var count = 100
         var page = 1
-        while count >= 1000 {
-            let sneakers = try await getDataFromStockX(keyWord: "", page: page, count: 1000)
+        while count > 0 {
+            let sneakers = try await getProductData(keyWord: "", page: page, count: 100)
             for i in 0..<sneakers.count {
-                let sneaker = Sneaker(id: sneakers[i].urlKey.absoluteString)
-                try await sneaker.create(on: req.db)
+                try await handleSneaker(with: sneakers[i], req: req)
             }
             count = sneakers.count
             page += 1
@@ -30,8 +29,33 @@ struct SneakersController: RouteCollection {
         return .ok
     }
 
+    private func handleSneaker(with sneakerDTO: SneakerDTO, req: Request) async throws -> Void {
+        if let sneaker = try await Sneaker.query(on: req.db)
+            .filter(\.$idStockX == sneakerDTO.urlKey)
+            .first() {
+
+            //update info in main table
+            sneaker.update(with: sneakerDTO)
+
+            //TODO update 360 represenation
+        } else {
+            let sneaker = Sneaker(sneakerDTO: sneakerDTO)
+
+            //create sneakers main table
+            try await sneaker.create(on: req.db)
+
+            //create 360 represenation
+            for i in 0..<sneakerDTO.images360.count - 1 {
+                let image = sneakerDTO.images360[i]
+                let sneaker360 = Sneaker360Presentation(sneakerID: sneaker.id, image: image)
+                try await sneaker360.create(on: req.db)
+            }
+
+        }
+    }
+
     func delete(req: Request) async throws -> HTTPStatus {
-        guard let sneaker = try await Sneaker.find(req.parameters.get("sneakerID"), on: req.db) else {
+        guard let sneaker = try await Sneaker.find(req.parameters.get("id"), on: req.db) else {
             throw Abort(.notFound)
         }
         try await sneaker.delete(on: req.db)
