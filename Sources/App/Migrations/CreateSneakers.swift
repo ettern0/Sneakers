bc0bf8e7216f9e4bf2ababa01f7e2aa499cf0fbc
ettern0 <evserdyukov@gmail.com>
2022-05-12 15:18:12 +0200

    Add new fields to Sneakers model

--- Sources/App/Migrations/CreateSneakers.swift
+++ Sources/App/Migrations/CreateSneakers.swift
@@ -4,10 +4,22 @@ struct CreateSneaker: AsyncMigration {
     func prepare(on database: Database) async throws {
         try await database.schema("sneakers")
             .id()
+            .field("idStockX", .string, .required)
+            .field("styleID", .string, .required)
+            .field("thumbnail", .string, .required)
+            .field("colorway", .string, .required)
+            .field("shoeName", .string, .required)
+            .field("description", .string, .required)
+            .create()
+        try await database.schema("sneakers360Presentation")
+            .id()
+            .field("sneakerID", .uuid, .required)
+            .field("image", .string, .required)
             .create()
     }
 
     func revert(on database: Database) async throws {
         try await database.schema("sneakers").delete()
+        try await database.schema("sneakers360Presentation").delete()
     }
 }
