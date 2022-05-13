0dbdba327dc580d8694bd43ab1c3d04989eda9c4
ettern0 <evserdyukov@gmail.com>
2022-05-13 10:48:36 +0200

    Fix comment

--- Sources/App/Migrations/CreateSneaker360Presentation.swift
+++ Sources/App/Migrations/CreateSneaker360Presentation.swift
@@ -1,8 +1,22 @@
 //
-//  File.swift
+//  CreateSneaker360Presentation.swift
 //  
 //
 //  Created by Evgeny Serdyukov on 13.05.2022.
 //
 
-import Foundation
+import Fluent
+
+struct CreateSneaker360Presentation: AsyncMigration {
+    func prepare(on database: Database) async throws {
+        try await database.schema("sneakers360Presentation")
+            .id()
+            .field("sneakerID", .uuid, .required)
+            .field("image", .string, .required)
+            .create()
+    }
+
+    func revert(on database: Database) async throws {
+        try await database.schema("sneakers360Presentation").delete()
+    }
+}
