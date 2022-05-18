d2e38f71c1c1c34ccb8e323dcf01c468d6048480
ettern0 <evserdyukov@gmail.com>
2022-05-18 23:17:11 +0200

    rename model Sneaker

---
+++ iOS/Sneakers/Models/Sneaker.swift
@@ -0,0 +1,18 @@
+//
+//  Sneakers.swift
+//  SneakersUIEffects
+//
+//  Created by Evgeny Serdyukov on 16.05.2022.
+//
+
+import Foundation
+import SwiftUI
+
+struct Sneaker: Identifiable, Codable, Hashable {
+    let id: UUID?
+    var thumbnail: String
+    var description: String
+    var shoeName: String
+}
+
+
