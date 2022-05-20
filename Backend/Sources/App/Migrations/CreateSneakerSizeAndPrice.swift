9968326b4babeb0285bd588eb2678b3d1bdca93f
ettern0 <evserdyukov@gmail.com>
2022-05-20 13:06:05 +0200

    Extend API

---
+++ Backend/Sources/App/Migrations/CreateSneakerSizeAndPrice.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  
+//
+//  Created by Evgeny Serdyukov on 20.05.2022.
+//
+
+import Fluent
+
+struct CreateSneakerSizeAndPrice: AsyncMigration {
+    func prepare(on database: Database) async throws {
+        try await database.schema("sneakerSizeAndPrice")
+            .id()
+            .field("sneakerID", .string, .required)
+            .field("shop", .string, .required)
+            .field("size", .string, .required)
+            .field("price", .double, .required)
+            .create()
+    }
+
+    func revert(on database: Database) async throws {
+        try await database.schema("sneakerSizeAndPrice").delete()
+    }
+}
+
