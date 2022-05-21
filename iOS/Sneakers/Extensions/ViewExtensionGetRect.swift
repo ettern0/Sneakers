08911b1db37ac5cc051d4b3eee198e049bc1d4fa
ettern0 <evserdyukov@gmail.com>
2022-05-21 21:43:10 +0200

    Implement get rect View extension

---
+++ iOS/Sneakers/Extensions/ViewExtensionGetRect.swift
@@ -0,0 +1,14 @@
+//
+//  ViewExtensions.swift
+//  Sneakers
+//
+//  Created by Evgeny Serdyukov on 17.05.2022.
+//
+
+import SwiftUI
+
+extension View {
+    func getRect() -> CGRect {
+        return UIScreen.main.bounds
+    }
+}
