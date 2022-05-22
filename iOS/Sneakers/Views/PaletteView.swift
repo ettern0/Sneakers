d162ae9cef31ea6a56beaa294dbfc2bae590544c
ettern0 <evserdyukov@gmail.com>
2022-05-22 16:51:45 +0200

    Implement optin to choose palette vertical or horizontal possition

--- iOS/Sneakers/Views/PaletteView.swift
+++ iOS/Sneakers/Views/PaletteView.swift
@@ -10,18 +10,27 @@ import SwiftUI
 struct PaletteView: View {
     @StateObject var viewModel: PaletteViewModel = PaletteViewModel.instance
     let addHeader: Bool
+    let position: Axis.Set
 
     var body: some View {
         VStack(spacing: 10) {
             if addHeader {
                 Text("Your Palette")
             }
-            HStack(spacing: 0) {
-                ForEach(viewModel.palette, id: \.self) { color in
-                    color
+            if position == .horizontal {
+                HStack(spacing: 0) {
+                    ForEach(viewModel.palette, id: \.self) { color in
+                        color
+                    }
+                }
+            } else {
+                VStack(spacing: 0) {
+                    ForEach(viewModel.palette, id: \.self) { color in
+                        color
+                    }
                 }
             }
-            .clipShape(RoundedRectangle(cornerRadius: 5, style: .continuous))
         }
+        .clipShape(RoundedRectangle(cornerRadius: 5, style: .continuous))
     }
 }
