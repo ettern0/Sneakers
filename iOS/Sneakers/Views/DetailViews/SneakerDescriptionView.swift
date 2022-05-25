047f9549dcf7f8fae0d34cf5e4da0b3d6098704c
ettern0 <evserdyukov@gmail.com>
2022-05-25 11:45:46 +0200

    Extend description view

--- iOS/Sneakers/Views/DetailViews/SneakerDescriptionView.swift
+++ iOS/Sneakers/Views/DetailViews/SneakerDescriptionView.swift
@@ -18,6 +18,8 @@ struct SneakerDescriptionView: View {
             Text("Price range: 120.000$  - 180.000$").italic().padding(.bottom, 15).opacity(0.5)
             Text("Description".capitalized).font(.title3).bold().padding(.bottom, 10)
             Text(sneaker.description.capitalized.replacingOccurrences(of: "<Br>", with: "")).bold()
+            Text(sneaker.description.capitalized.replacingOccurrences(of: "<Br>", with: "")).bold()
+            Text(sneaker.description.capitalized.replacingOccurrences(of: "<Br>", with: "")).bold()
         }
         .padding()
         .background(Color.white)
