7ad5adb9df30905897adceb75c911439f5932008
ettern0 <evserdyukov@gmail.com>
2022-05-26 10:53:15 +0200

    Fix description view

--- iOS/Sneakers/Views/DetailViews/SneakerDescriptionView.swift
+++ iOS/Sneakers/Views/DetailViews/SneakerDescriptionView.swift
@@ -13,13 +13,26 @@ struct SneakerDescriptionView: View {
 
     var body: some View {
         VStack(alignment: .leading, spacing: 5) {
-            Text(sneaker.brand.capitalized).font(.title3).opacity(0.8)
-            Text(sneaker.name.capitalized).font(.largeTitle).bold()
-            Text("Price range: 120.000$  - 180.000$").italic().padding(.bottom, 15).opacity(0.5)
-            Text("Description".capitalized).font(.title3).bold().padding(.bottom, 10)
-            Text(sneaker.description.capitalized.replacingOccurrences(of: "<Br>", with: "")).bold()
-            Text(sneaker.description.capitalized.replacingOccurrences(of: "<Br>", with: "")).bold()
-            Text(sneaker.description.capitalized.replacingOccurrences(of: "<Br>", with: "")).bold()
+            Text(sneaker.brand.capitalized).font(.system(size: 17)).opacity(0.8)
+            HStack {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(sneaker.name.capitalized).font(.system(size: 22)).bold()
+                    Text("Price range: 120.000$  - 180.000$").font(.system(size: 12)).italic().padding(.bottom, 15).opacity(0.5)
+                }
+                Spacer()
+                Button {
+
+                } label: {
+                    Image(systemName: "heart")
+                        .resizable()
+                        .frame(width: 32, height: 32)
+                        .foregroundColor(.red)
+                        .padding(.bottom)
+                }
+            }
+            Text("Description".capitalized).font(.system(size: 15)).bold().padding(.bottom, 10)
+            Text(sneaker.description.capitalized.replacingOccurrences(of: "<Br>", with: "")).font(.system(size: 12))
+            Text(sneaker.description.capitalized.replacingOccurrences(of: "<Br>", with: "")).font(.system(size: 12))
         }
         .padding()
         .background(Color.white)
