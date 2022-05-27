d24cf781d8ba9213c666c4ea6ccf1e83344466ee
ettern0 <evserdyukov@gmail.com>
2022-05-27 16:10:47 +0200

    Adapt Palete view to new model

--- iOS/Sneakers/Views/PaletteView.swift
+++ iOS/Sneakers/Views/PaletteView.swift
@@ -8,34 +8,16 @@
 import SwiftUI
 
 struct PaletteView: View {
-    @StateObject var viewModel: PaletteViewModel = PaletteViewModel.instance
-    let addHeader: Bool
-    let position: Axis.Set
-
+    @StateObject var viewModel: PaletteViewModel
+    // let position: Axis.Set
     let paletteHeight: CGFloat = 12
 
     var body: some View {
-        GeometryReader {
-        VStack(spacing: 10) {
-            if addHeader {
-                Text("Your Palette")
+        HStack(spacing: 10) {
+            ForEach(viewModel.palette, id: \.self) { color in
+                color
+                    .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
             }
-            if position == .horizontal {
-                HStack(spacing: 4) {
-                    ForEach(viewModel.palette, id: \.self) { color in
-                        color
-                    }
-                }
-            } else {
-                VStack(spacing: 0) {
-                    ForEach(viewModel.palette, id: \.self) { color in
-                        color
-                    }
-                }
-            }
-        }
-        .clipShape(RoundedRectangle(cornerRadius: 5, style: .continuous))
-        .frame(width: $0.size.width * 0.6, height: paletteHeight)
         }
     }
 }
