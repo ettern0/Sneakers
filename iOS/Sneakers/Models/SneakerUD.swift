364be5dd4c8530e9b266409f588eece2dff3fb04
ettern0 <evserdyukov@gmail.com>
2022-05-27 16:08:24 +0200

    Implement SneakerUD model to store in User defaults

---
+++ iOS/Sneakers/Models/SneakerUD.swift
@@ -0,0 +1,21 @@
+//
+//  SneakerUD.swift
+//  Sneakers
+//
+//  Created by Evgeny Serdyukov on 27.05.2022.
+//
+
+import Foundation
+
+struct SneakerUD: Codable, Identifiable {
+
+    let id: String
+    let thumbnail: String
+    let name: String
+
+    init(from sneaker: Sneaker) {
+        self.id = sneaker.id
+        self.thumbnail = sneaker.thumbnail
+        self.name = sneaker.name
+    }
+}
