27aadf6dda566db37f637f4307f32a4a4fce0b69
ettern0 <evserdyukov@gmail.com>
2022-06-14 01:47:57 +0200

    Implement detail view from favorites

--- iOS/Sneakers/Utilities/Constants.swift
+++ iOS/Sneakers/Utilities/Constants.swift
@@ -8,12 +8,13 @@
 import Foundation
 
 enum Constants {
-//    static let baseURL = "https://vapor-sneakers.herokuapp.com/"
-    static let baseURL = "http://127.0.0.1:8080/"
+    static let baseURL = "https://vapor-sneakers.herokuapp.com/"
+    // static let baseURL = "http://127.0.0.1:8080/"
 }
 
 enum Endpoints {
     static let all = "sneakers/all"
     static let portion = "sneakers/portion/20"
+    static let portionID = "sneakers/portion/?id="
     static let filter = "sneakers/filters/palette?palette="
 }
